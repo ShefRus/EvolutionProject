699b38dee7b334dcbda737b3d3fd7b8fc711c2a6
ShefRus <ruslan1998375@gmail.com>
2021-06-13 03:26:12 +0300

    First commit

---
+++ Assets/Node.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+
+    public int nodeValue;
+
+    public enum NodeType
+    {
+        dynamics,
+        mechanica,
+        automatics
+    }
+
+    public List<NodeType> unlockedNodes;
+    public List<NodeType> nodesRequirament;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
